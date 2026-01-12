3e4aa4a4664fdca424aa60ae053a0eba1063f720
ashu <kurvheashutosh@gmail.com>
2026-01-12 23:07:25 +0530

    Pattern #2 – Inverted Right-Angled Triangle

---
+++ patterns/Inverted-Right-Angled.js
@@ -0,0 +1,16 @@
+// Pattern #2 – Inverted Right-Angled Triangle
+
+for (let i = 4; i >= 1; i--) {
+  let row = "";
+  for (let j = 1; j <= i; j++) {
+    row += "*";
+  }
+  console.log(row);
+}
+
+// Output:
+
+// ****
+// ***
+// **
+// *
