3e03fa2e76e97ea64586ec91f1d818056d60105c
ashu <kurvheashutosh@gmail.com>
2026-01-12 23:10:20 +0530

    Pattern #9 – Reverse Number Triangle

---
+++ patterns/Reverse-Number-Tringle.js
@@ -0,0 +1,17 @@
+// Pattern #9 – Reverse Number Triangle
+
+for (let i = 4; i >= 1; i--) {
+  let row = "";
+  for (let j = 1; j <= i; j++) {
+    row += j;
+  }
+  console.log(row);
+}
+
+
+// Output;
+
+// 1234;
+// 123;
+// 12;
+// 1;
