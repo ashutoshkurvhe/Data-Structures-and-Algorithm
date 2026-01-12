cf2a8308cf222d2da235d4b428756624a0390efa
ashu <kurvheashutosh@gmail.com>
2026-01-12 23:09:22 +0530

    Pattern #6 – Right-Aligned Star Triangle (Spaces + Stars)

---
+++ patterns/Right-Aligned-Star-Triangle.js
@@ -0,0 +1,19 @@
+// Pattern #6 – Right-Aligned Star Triangle (Spaces + Stars)
+
+for (let i = 1; i <= 4; i++) {
+  let row = "";
+  for (let s = 1; s <= 4 - i; s++) {
+    row += " ";
+  }
+  for (let j = 1; j <= i; j++) {
+    row += "*";
+  }
+  console.log(row);
+}
+
+// Output:
+
+//    *
+//   **
+//  ***
+// ****
