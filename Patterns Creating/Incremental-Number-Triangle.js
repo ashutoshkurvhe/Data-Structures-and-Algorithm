62b8d219459517dd95a1b74984c595aa5d9a8b05
ashu <kurvheashutosh@gmail.com>
2026-01-14 21:59:07 +0530

    Adding Incremental Number Triangle Pattern

---
+++ Patterns Creating/Incremental-Number-Triangle.js
@@ -0,0 +1,19 @@
+// Expected Output
+
+// 1
+// 2 3
+// 4 5 6
+// 7 8 9 10
+
+
+let count = 1;
+
+for (let i = 1; i <= 4; i++){
+    let row = "";
+    for (let j = 1; j <= i; j++){
+        row += count + "";
+        count++;
+    }
+    console.log(row.trim());
+}
+
