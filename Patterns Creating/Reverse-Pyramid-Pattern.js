78d678d51e55ed96a1a9820296a8dfedbd416bc4
ashu <kurvheashutosh@gmail.com>
2026-01-16 19:22:11 +0530

    Adding Reverse Pyramid Pattern

---
+++ Patterns Creating/Reverse-Pyramid-Pattern.js
@@ -0,0 +1,26 @@
+//Expected Output
+
+// *******
+//  *****
+//   ***
+//    *
+
+
+
+for (let i = 4; i >= 1; i--) {
+  let row = "";
+  for (let j = 4; j > i; j--) {
+    row += " ";
+  }
+
+  for (let k = 2 * i - 1; k >= 1; k--) {
+    row += "*";
+  }
+  console.log(row);
+}
+
+
+// i = 4 j = 4 to 4 => ""; k = 1 to 7 => "*******"
+// i = 3 j = 4 to 3 => " "; k = 1 to 5 => "*****"
+// i = 2 j = 4 to 2 => "  "; k = 1 to 3 => "***"
+// i = 1 j = 4 to 1 => "   "; k = 1 to 1 => "*"
