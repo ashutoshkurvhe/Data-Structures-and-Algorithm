b9f66cde90d36b598f0527bfbd4a059cf12086ab
ashu <kurvheashutosh@gmail.com>
2026-01-17 23:51:11 +0530

    Adding Hollow Hourglass Pattern

---
+++ Patterns Creating/Hollow-Hourglass-Pattern.js
@@ -0,0 +1,49 @@
+//Expected Pattern
+
+
+// *******
+//  *   *
+//   * *
+//    *
+//   * *
+//  *   *
+// *******
+
+
+
+totalRows = 4;
+for (let i = totalRows; i >= 1; i--){
+    let rows = "";
+
+    for (let j = 1; j <= totalRows - i;  j++){
+        rows += " ";
+    }
+
+    for (let k = 1; k <= 2 * i - 1; k++){
+        if (i === totalRows || k === 1 || k === 2 * i -1) {
+            rows += "*";
+        } else {
+            rows += " ";
+        }
+    }
+
+    console.log(rows);
+}
+
+for (let i = 2; i <= totalRows; i++){
+    let rows = "";
+
+    for (let j = 1; j <= totalRows - i;  j++){
+        rows += " ";
+    }
+
+    for (let k = 1; k <= 2 * i - 1; k++){
+        if (i === totalRows || k === 1 || k === 2 * i -1) {
+            rows += "*";
+        } else {
+            rows += " ";
+        }
+    }
+
+    console.log(rows);
+}
