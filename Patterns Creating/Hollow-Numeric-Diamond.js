24ad0de4bf2508c3705a12692e11a92d217fccbd
ashu <kurvheashutosh@gmail.com>
2026-01-17 23:52:10 +0530

    Adding Hollow Numeric Diamond

---
+++ Patterns Creating/Hollow-Numeric-Diamond.js
@@ -0,0 +1,44 @@
+//Expected Pattern
+
+
+//    1
+//   1 1
+//  1   1
+// 1     1
+//  1   1
+//   1 1
+//    1
+
+
+let totalRows = 4;
+for (let i = 1; i <= totalRows; i++) {
+  let rows = "";
+  for (let j = 1; j <= totalRows - i; j++) {
+    rows += " ";
+  }
+
+  for (let k = 1; k <= 2 * i - 1; k++) {
+    if (i === 1 || k === 1 || k === 2 * i - 1) {
+      rows += "1";
+    } else {
+      rows += " ";
+    }
+  }
+  console.log(rows);
+}
+
+for (let i = totalRows - 1; i >= 1; i--) {
+  let rows = "";
+  for (let j = totalRows - i; j >= 1; j--) {
+    rows += " ";
+  }
+
+  for (let k = 2 * i - 1; k >= 1; k--) {
+    if (i === 1 || k === 1 || k === 2 * i - 1) {
+      rows += "1";
+    } else {
+      rows += " ";
+    }
+  }
+  console.log(rows);
+}
