c49dac1391ded18efb333f0dd28023e18aff16c8
ashu <kurvheashutosh@gmail.com>
2026-01-17 23:50:39 +0530

    Hollow Hourglass Pattern

---
+++ Patterns Creating/Hollow-Diamond-Pattern.js
@@ -0,0 +1,43 @@
+//Expected Pattern
+
+//     *
+//    * *
+//   *   *
+//  *     *
+//   *   *
+//    * *
+//     *
+
+
+let totalRows = 4;
+for (let i = 1; i <= totalRows; i++) {
+  let rows = "";
+  for (let j = 1; j <= totalRows - i; j++) {
+    rows += " ";
+  }
+
+  for (let k = 1; k <= 2 * i - 1; k++) {
+    if (i === 1 || k === 1 || k === 2 * i - 1) {
+      rows += "*";
+    } else {
+      rows += " ";
+    }
+  }
+  console.log(rows);
+}
+
+for (let i = totalRows - 1; i >= 1; i--) {
+  let rows = "";
+  for (let j = totalRows - i; j >= 1; j--) {
+    rows += " ";
+  }
+
+  for (let k = 2 * i - 1; k >= 1; k--) {
+    if (i === 1 || k === 1 || k === 2 * i - 1) {
+      rows += "*";
+    } else {
+      rows += " ";
+    }
+  }
+  console.log(rows);
+}
