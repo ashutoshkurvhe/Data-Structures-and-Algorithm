4d2d925d3683ce3eca54a946818ad98613ae663d
ashu <kurvheashutosh@gmail.com>
2026-01-17 23:56:38 +0530

    Rename The file

---
+++ Patterns Creating/Hollow-Pyramid.js
@@ -0,0 +1,24 @@
+//Expected Pattern
+
+//     *
+//    * *
+//   *   *
+//  *     *
+// *********
+
+let totalRows = 5;
+for (let i = 1; i <= totalRows; i++){
+    let rows = "";
+    for (let j = 1; j <= totalRows - i; j++){
+        rows += " ";
+    }
+
+    for (let k = 1; k <= 2 * i - 1; k++){
+        if (i === 1 || i === totalRows || k === 1 || k === 2 * i - 1) {
+            rows += "*"
+        } else {
+            rows += " "
+        }
+    }
+    console.log(rows)
+}
