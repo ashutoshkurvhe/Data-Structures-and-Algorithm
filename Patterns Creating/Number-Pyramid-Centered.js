cb2db0191f7067c90ada624bc07f9062302d1bf4
ashu <kurvheashutosh@gmail.com>
2026-01-17 23:33:19 +0530

    Adding changes

--- Patterns Creating/Number-Pyramid-Centered.js
+++ Patterns Creating/Number-Pyramid-Centered.js
@@ -4,3 +4,22 @@
 //     121;
 //    12321;
 //   1234321;
+
+
+let totalRows = 4;
+
+for (let i = 1; i <= totalRows; i++){
+    let rows = "";
+    for (let j = 1; j <= totalRows - i; j++){
+        rows += " ";
+    }
+
+    for (let k = 1; k <= i; k++){
+        rows += k
+    }
+
+    for (let l = i - 1; l >= 1; l--){
+        rows += l
+    }
+    console.log(rows)
+}
