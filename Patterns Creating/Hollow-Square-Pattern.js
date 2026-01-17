1d1169815d6a67f2500e60a690e7a7f9d45da375
ashu <kurvheashutosh@gmail.com>
2026-01-17 23:52:34 +0530

    Adding Hollow Square Pattern

---
+++ Patterns Creating/Hollow-Square-Pattern.js
@@ -0,0 +1,20 @@
+//Expected Pattern
+
+// ****
+// *  *
+// *  *
+// ****
+
+
+let totalRows = 4;
+for (let i = 1; i <= totalRows; i++){
+    let rows = "";
+    for (let j = 1; j <= totalRows; j++){
+        if (i === 1 || i === totalRows || j === 1 || j === totalRows) {
+            rows += "*";
+        } else {
+            rows += " ";
+        }
+    }
+    console.log(rows);
+}
