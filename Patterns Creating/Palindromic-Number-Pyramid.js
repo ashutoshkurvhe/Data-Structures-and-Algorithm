121f03583212745951f97fbb60c4f37fb60f2a07
ashu <kurvheashutosh@gmail.com>
2026-01-19 20:09:36 +0530

    Adding Palindromic Number Pyramid

---
+++ Patterns Creating/Palindromic-Number-Pyramid.js
@@ -0,0 +1,26 @@
+//Expected Pattern
+
+
+//    1;
+//   121;
+//  12321;
+// 1234321;
+
+
+let totalRows = 4;
+for (let i = 1; i <= totalRows; i++){
+    let rows = "";
+
+    for (let j = 1; j <= totalRows - i; j++){
+        rows += " ";
+    }
+
+    for (let k = 1; k <= i; k++){
+        rows += k;
+    }
+
+    for (let l = i - 1; l >= 1; l--){
+        rows += l;
+    }
+    console.log(rows);
+}
