2d6c3de45900beacd75b7610026943a4586c2150
ashu <kurvheashutosh@gmail.com>
2026-01-19 20:08:36 +0530

    Adding Mixed Boundary Pattern

---
+++ Patterns Creating/Mixed-Boundary-Pattern.js
@@ -0,0 +1,47 @@
+//Expected Patterns
+
+// *****
+// *   *
+// *****
+// *   *
+// *****
+
+
+//Wrong way hard coded i === totalRows - 2
+// let totalRows = 5;
+// for (let i = 1; i <= totalRows; i++){
+//     let rows = "";
+//     for (let j = 1; j <= totalRows; j++){
+//         if (i === 1 || i === totalRows || i === totalRows - 2 || j === 1 || j === totalRows) {
+//             rows += "*";
+//         } else {
+//             rows += " "
+//         }
+
+//     }
+//     console.log(rows)
+// }
+
+
+//Correct way formula Based
+
+let totalRows = 5;
+
+for (let i = 1; i <= totalRows; i++) {
+  let row = "";
+
+  for (let j = 1; j <= totalRows; j++) {
+    if (
+      i === 1 || // top
+      i === totalRows || // bottom
+      j === 1 || // left
+      j === totalRows || // right
+      i === Math.ceil(totalRows / 2) // middle row
+    ) {
+      row += "*";
+    } else {
+      row += " ";
+    }
+  }
+  console.log(row);
+}
