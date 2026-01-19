76a770f2ac8a5b6328a888d10ed0235f2908b0f3
ashu <kurvheashutosh@gmail.com>
2026-01-19 20:10:43 +0530

    Adding X Pattern js

---
+++ Patterns Creating/X-Pattern.js
@@ -0,0 +1,62 @@
+//Expected Pattern
+
+// *   *
+//  * *
+//   *
+//  * *
+// *   *
+
+
+
+let totalRows = 3;
+
+
+for (let i = totalRows; i >= 1; i--) {
+  let rows = "";
+  for (let j = totalRows - i; j >= 1; j--) {
+    rows += " ";
+  }
+
+  for (let k = 2 * i - 1; k >= 1; k--) {
+    if (i === 1 || k === 1 || k === 2 * i - 1) {
+      rows += "*";
+    } else {
+      rows += " ";
+    }
+  }
+  console.log(rows);
+}
+
+for (let i = 2; i <= totalRows; i++) {
+  let rows = "";
+  for (let j = 1; j <= totalRows - i; j++) {
+    rows += " ";
+  }
+
+  for (let k = 1; k <= 2 * i - 1; k++) {
+    if (i === 1 || k === 1 || k === 2 * i - 1) {
+      rows += "*";
+    } else {
+      rows += " ";
+    }
+  }
+  console.log(rows);
+}
+
+
+// let totalRows = 3;
+// for (let i = 1; i <= totalRows; i++){
+//     let rows = "";
+//     for (let j = 1; j <= i; j++){
+//         rows += "1";
+//     }
+
+//     for (let k = 2 * i - 1; k >= 1; k--){
+//         if (k === 1 || k === (2 * i - 1)) {
+//             rows += "*";
+//         } else {
+//             rows += " ";
+//         }
+//     }
+//     console.log(rows);
+// }
