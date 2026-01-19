9694dffe7b9e67dee0dad932d498864d18cdf721
ashu <kurvheashutosh@gmail.com>
2026-01-19 20:08:55 +0530

    Adding Number Pyramid

---
+++ Patterns Creating/Number-Pyramid.js
@@ -0,0 +1,26 @@
+//Expected Patterns
+
+//    1
+//   1 1
+//  1   1
+// 1111111
+
+let totalRows = 4;
+
+for (let i = 1; i <= totalRows; i++){
+    let rows = "";
+
+    for (let j = 1; j <= totalRows - i; j++){
+        rows += " ";
+    }
+
+    for (let k = 1; k <= 2 * i - 1; k++){
+        if (k === 1 || k === 2 * i - 1 || i === totalRows) {
+            rows += "1";
+        } else {
+            rows += " ";
+        }
+    }
+
+    console.log(rows);
+}
