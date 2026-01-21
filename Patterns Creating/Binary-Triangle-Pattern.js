5ac841ba2adb0e085ccb79c1698b07d7bf302aed
ashu <kurvheashutosh@gmail.com>
2026-01-21 22:50:17 +0530

    Adding Output

--- Patterns Creating/Binary-Triangle-Pattern.js
+++ Patterns Creating/Binary-Triangle-Pattern.js
@@ -1,20 +1,22 @@
+// Binary Triangle Pattern
+
+let totalRows = 4;
+
+for (let i = 1; i <= totalRows; i++) {
+  let rows = "";
+  for (let j = 1; j <= i; j++) {
+    if ((i + j) % 2 === 0) {
+      rows += 1;
+    } else {
+      rows += 0;
+    }
+  }
+  console.log(rows);
+}
+
 //Expected Pattern
 
 // 1;
 // 01;
 // 101;
 // 0101;
-
-let totalRows = 4;
-
-for (let i = 1; i <= totalRows; i++){
-    let rows = "";
-    for (let j = 1; j <= i; j++){
-        if ((i + j) % 2 === 0) {
-            rows += 1;
-        } else {
-            rows += 0;
-        }
-    } 
-    console.log(rows);
-}
