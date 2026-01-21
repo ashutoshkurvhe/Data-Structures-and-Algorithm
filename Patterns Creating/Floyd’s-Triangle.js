bdbae19ac59618e986edc8861cfe8c5d3073fe5f
ashu <kurvheashutosh@gmail.com>
2026-01-21 22:50:22 +0530

    Adding Output

--- Patterns Creating/Floyd’s-Triangle.js
+++ Patterns Creating/Floyd’s-Triangle.js
@@ -1,9 +1,4 @@
-//Expected Output
-
-// 1;
-// 23;
-// 456;
-// 78910;
+// Floyd’s Triangle Pattern
 
 let count = 1;
 
@@ -22,3 +17,11 @@ for (let i = 1; i <= 4; i++){
 // i = 1 j = 1 to 2 => 23
 // i = 1 j = 1 to 3 => 456
 // i = 1 j = 1 to 4 => 78910
+
+
+//Expected Output
+
+// 1;
+// 23;
+// 456;
+// 78910;
